2720ca3cf24778fdc3deed1000e14018a7aae6ad
othamerfortini <fortini.thamer@gmail.com>
2026-08-23 20:27:15 +0200

    make content pages part of the manifestation visual system

--- instinct13-web/components/ui/ContentPage.tsx
+++ instinct13-web/components/ui/ContentPage.tsx
@@ -9,8 +9,13 @@ export function ContentPage({ children }: ContentPageProps) {
   return (
     <LayoutContainer navigationVisible>
       <PageTransition>
-        <div className="mx-auto max-w-2xl px-6 pb-24 pt-32 sm:px-8 sm:pt-36 md:pb-32">
-          {children}
+        <div className="site-page">
+          <div aria-hidden="true" className="site-field site-field-left" />
+          <div aria-hidden="true" className="site-field site-field-right" />
+          <div aria-hidden="true" className="site-observer" />
+          <div className="site-content mx-auto max-w-4xl px-6 pb-32 pt-32 sm:px-8 sm:pt-40 md:pb-40">
+            {children}
+          </div>
         </div>
       </PageTransition>
     </LayoutContainer>
